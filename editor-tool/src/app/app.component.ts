e7992cdcf7243d146271333d1748437c4967b7d0
Bharathi AR <rachanabharathi1997@gmail.com>
2024-12-18 19:15:03 +0530

    initial commit angular code

---
+++ editor-tool/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  text: string = '';
+  findWord: string = '';
+  replaceWord: string = '';
+  replacedText: string = '';
+
+  replaceText(): void {
+    if (this.findWord) {
+      const regex = new RegExp(this.findWord, 'g');
+      this.replacedText = this.text.replace(regex, this.replaceWord);
+    } else {
+      this.replacedText = this.text;
+    }
+  }
+}
